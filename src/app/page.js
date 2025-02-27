0e93023514c5276ae3e74b7e8322da6c9dc7055a
Suhacan Kilic <kilicsuha@gmail.com>
2025-02-27 22:51:25 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,139 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { languages } from "./languages";
+import React from "react";
+import clsx from "clsx";
+import Confetti from 'react-confetti'
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // Alphabet
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  // React States
+  const [word, setWord] = React.useState("react");
+  const [guessedLetters, setGuessedLetters] = React.useState([]);
+
+  // Some Status Of The Game
+  const wrongGuessNumber = guessedLetters.filter(
+    (el) => !word.includes(el)
+  ).length;
+  const isGameLost = wrongGuessNumber >= languages.length - 1;
+  const isGameWon = word.split("").every((el) => guessedLetters.includes(el));
+  const isGameOver = isGameWon || isGameLost;
+
+  // newGame
+  function newGame() {
+    setWord("react");
+    setGuessedLetters([]);
+  }
+
+  //gameStatusClass
+  const gameStatusClass = clsx("game-status-section", {
+    "game-won": isGameWon,
+    "game-lost": isGameLost,
+  });
+
+  // gameStatus element
+  function gameStatus() {
+    if (isGameWon) {
+      return (
+        <>
+          <h1>You won!</h1>
+          <p>Well done.</p>
+        </>
+      );
+    } else if (isGameLost) {
+      return (
+        <>
+          <h1>You lost!</h1>
+          <p>Don't give up.</p>
+        </>
+      );
+    } else {
+      return null;
+    }
+  }
+
+
+  // Keyboard Click Event
+  function handleKeyboardClick(el) {
+    setGuessedLetters((prev) => [...new Set([...prev, el])]);
+  }
+
+  // Displayed Programming Languages
+  const languageElements = languages.map((el, index) => {
+    // When guess is wrong, change the style of the language
+    const isLangStyleChanged = index < wrongGuessNumber;
+    const className = clsx({
+      "deleted-language": isLangStyleChanged,
+    });
+    return (
+      <span
+        style={{ backgroundColor: el.backgroundColor, color: el.color }}
+        key={el.name}
+        className={className}
+      >
+        {el.name}
+      </span>
+    );
+  });
+
+  // Given Word
+  const wordElement = word.split("").map((el) => {
+    const shouldShowLetter = isGameLost || guessedLetters.includes(el);
+
+    const className = clsx({
+      "green-letters": isGameOver && guessedLetters.includes(el),
+      "red-letters": isGameOver && !guessedLetters.includes(el),
+    });
+
+    return (
+      <span className={className} key={el}>
+        {shouldShowLetter ? el.toUpperCase() : ""}
+      </span>
+    );
+  });
+
+  // Keyboard
+  const keyboardElement = alphabet.split("").map((el) => {
+    const isLetterInWord = word.includes(el);
+    const isLetterGuessed = guessedLetters.includes(el);
+
+    const className = clsx({
+      "correct-guess": isLetterGuessed && isLetterInWord,
+      "incorrect-guess": isLetterGuessed && !isLetterInWord,
+    });
+
+    return (
+      <button
+        key={el}
+        className={className}
+        onClick={() => handleKeyboardClick(el)}
+      >
+        {el.toLocaleUpperCase()}
+      </button>
+    );
+  });
+
+  return (
+    <main>
+      {isGameWon && <Confetti/>}
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className={gameStatusClass}>{gameStatus()}</section>
+      <section className="languages-section">{languageElements}</section>
+      <section className="word-section">{wordElement}</section>
+      <section className="keyboard-section">{keyboardElement}</section>
+      {isGameOver ? (
+        <button onClick={newGame} className="new-game-button">
+          New Game
+        </button>
+      ) : null}
+    </main>
   );
 }
